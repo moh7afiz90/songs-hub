36a94a433e70028947e1ef24a71025e2c57c11ba
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-16 01:09:12 +0300

    Added and configured Register component to the backend server

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Register from '@/components/register'
 
 Vue.use(Router)
 
@@ -10,6 +11,13 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      // navigation path
+      path: '/register',
+      name: 'register',
+      // Displaying Component
+      component: Register
     }
   ]
 })
