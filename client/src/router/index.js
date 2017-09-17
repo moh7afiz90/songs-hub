ef96a6cad11a5ce923f44bb63a5fc039f4c11866
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-17 21:28:45 +0300

    added navigation to the Toolbar

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Register from '@/components/register'
+import Login from '@/components/login'
 
 Vue.use(Router)
 
@@ -9,15 +10,24 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: 'root',
       component: Hello
     },
     {
       // navigation path
       path: '/register',
+      // Used for routing
       name: 'register',
       // Displaying Component
       component: Register
+    },
+    {
+      // navigation path
+      path: '/login',
+      // Used for routing
+      name: 'login',
+      // Displaying Component
+      component: Login
     }
   ]
 })
--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -4,5 +4,8 @@ import API from '@/services/API'
 export default {
   register (credentials) {
     return API().post('register', credentials)
+  },
+  login (credentials) {
+    return API().post('login', credentials)
   }
 }
