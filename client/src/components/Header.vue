6ddd5b5fe629a44257d41e244c5b76cd33dfd79a
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-17 06:21:55 +0300

    added navigation to the Toolbar

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -2,7 +2,7 @@
   <!-- fixed in all down the page -->
   <v-toolbar fixed class="blue darken-3">
       <v-toolbar-title class="mr-4">
-         GuitarTabTracker
+         <span class="home" @click="navigateTo({name: 'root'})">SongsHub</span>
       </v-toolbar-title>
       <v-toolbar-item>
           <v-btn flat light>Browse</v-btn>
@@ -10,18 +10,28 @@
       <v-spacer></v-spacer>
       <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
       <v-toolbar-item class="hidden-sm-and-down">
-          <v-btn>Login</v-btn>
-          <v-btn>Sign Up</v-btn>
+          <v-btn @click="navigateTo('')">Login</v-btn>
+          <v-btn @click="navigateTo({name: 'register'})">Sign Up</v-btn>
       </v-toolbar-item>
   </v-toolbar>
 </template>
 
 <script>
 export default {
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .home{
+    cursor: pointer;
+  }
+  .home:hover {
+    color: grey;
+  }
 </style>
