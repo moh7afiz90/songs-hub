0beae4560a2e6ca825c8eb5805e579f219739b76
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-19 00:29:56 +0300

    Setup Songs component to fetch songs from the backend

--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,6 +2,11 @@
   <v-layout column>
    <v-flex xs6 offset-xs3>
      <panel title="Songs">
+       <div v-for="song in songs" :key="song.title">
+        <li>{{song.title}}</li>
+        <li>{{song.artist}}</li>
+        <li>{{song.album}}</li>
+       </div>
      </panel>
    </v-flex>
   </v-layout>
@@ -12,6 +17,14 @@ import Panel from '@/components/Panel'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  mounted () {
+    // do a request to the backend for all the songs
   }
 }
 </script>
