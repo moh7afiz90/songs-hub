df68523b13aebbadd9abcafd54d4053b833e67ac
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-19 07:16:58 +0300

    Added google material icons CDN

--- client/src/components/Panel.vue
+++ client/src/components/Panel.vue
@@ -3,6 +3,10 @@
         <v-toolbar flat dense class="blue darken-3" dark>
           <v-toolbar-title>{{title}}</v-toolbar-title>
         </v-toolbar>
+
+            <v-btn>
+              <v-icon>Add</v-icon>
+            </v-btn>
             <div class="pl-4 pr-4 pt-2 pb-2">
               <slot>
                   No slot content defined.
