f86d4adb366c3248c3fd6f7ca5515981018d1813
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-25 22:18:07 +0300

    Added Songs Folder

---
+++ client/src/components/Songs/SongsPanel.vue
@@ -0,0 +1,113 @@
+<template>
+    <panel title="Songs List">
+        <v-btn 
+        slot="action"
+        @click="navigateTo({name: 'songs-create'})"
+        class="white"
+        light
+        medium
+        fab
+        right
+        middle
+        absolute
+        >
+        <i class="material-icons">add</i>
+        </v-btn>
+        <!--  -->
+    <v-container grid-list-xs6 text-xs-center class="blue darken-1">
+    <v-layout row wrap>
+        <v-flex 
+        xs6
+        v-for="song in songs"
+        :key="song.id"
+        >
+        <v-card 
+            dark 
+            class="white song-card"
+            >
+            <v-card-media>
+            <img :src="song.albumImageUrl" height="300px" alt="">
+            </v-card-media>
+            <v-card-title class="headline black--text px-4">
+            <h5><strong>{{song.title}}</strong></h5>
+            </v-card-title>
+            <div class="headline black--text">{{song.artist}}</div>
+            <span class="grey--text">{{song.genre}}</span>
+            <v-card-actions>
+            <v-spacer></v-spacer>
+            <div class="text-xs-center">
+                <v-btn 
+                class="text-xs-center"
+                @click="navigateTo({
+                name: 'song', 
+                    params: {
+                    songId: song.id
+                    }
+                })"
+                >
+                View
+                <br>
+                <i class="material-icons" large> pageview</i>
+            </v-btn>
+            </div>
+            </v-card-actions>
+        </v-card>
+        </v-flex>
+    </v-layout>
+    </v-container>
+    </panel>
+</template>
+
+<script>
+import SongsService from '@/services/SongsService'
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
+  async mounted () {
+    // do a request to the backend for all the songs
+    this.songs = (await SongsService.index()).data
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .song {
+  padding: 20px;
+   height: 330px;
+   overflow: hidden;
+ }
+ 
+ .song-title {
+   font-size: 30px;
+ }
+  .song-artist {
+    font-size: 24px;
+  }
+  
+  .song-genre {
+   font-size: 18px;
+  }
+  
+  .album-image {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    margin: 0 auto;
+  }
+  .song-card{
+    margin: 20px;
+  }
+</style>
--- client/src/components/Songs/index.vue
+++ client/src/components/Songs/index.vue
@@ -1,73 +1,19 @@
 <template>
   <v-layout row wrap>
    <v-flex xs8 offset-xs2>
-     <panel title="Songs List">
-          <v-btn 
-            slot="action"
-            @click="navigateTo({name: 'songs-create'})"
-            class="white"
-            light
-            medium
-            fab
-            right
-            middle
-            absolute
-          >
-          <i class="material-icons">add</i>
-          </v-btn>
-          <!--  -->
-      <v-container grid-list-xs6 text-xs-center class="blue darken-1">
-        <v-layout row wrap>
-          <v-flex 
-            xs6
-            v-for="song in songs"
-            :key="song.id"
-            >
-            <v-card 
-              dark 
-              class="white song-card"
-              >
-              <v-card-media>
-                <img :src="song.albumImageUrl" height="300px" alt="">
-              </v-card-media>
-              <v-card-title class="headline black--text px-4">
-                <h5><strong>{{song.title}}</strong></h5>
-              </v-card-title>
-              <div class="headline black--text">{{song.artist}}</div>
-              <span class="grey--text">{{song.genre}}</span>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <div class="text-xs-center">
-                  <v-btn 
-                   class="text-xs-center"
-                   @click="navigateTo({
-                    name: 'song', 
-                      params: {
-                      songId: song.id
-                      }
-                    })"
-                   >
-                  View
-                  <br>
-                  <i class="material-icons" large> pageview</i>
-                </v-btn>
-                </div>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-     </panel>
+     <songs-panel/>
    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SongsPanel from './SongsPanel'
 import SongsService from '@/services/SongsService'
 import Panel from '@/components/Panel'
 export default {
   components: {
-    Panel
+    Panel,
+    SongsPanel
   },
   data () {
     return {
